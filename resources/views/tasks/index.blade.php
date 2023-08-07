8b26c6ec25c54ffae5382b5078cef800650692b3
admin <a.michkire@gmail.com>
2023-08-08 01:01:53 +0300

    view add error list and old value

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,10 +2,11 @@
 
 @section('content')
     <h2>All tasks</h2>
+    @include('common.errors')
     <form action="/tasks" method="post">
         {{ csrf_field() }}
         <label>Task name:
-            <input type="text" name="name">
+            <input type="text" name="name" value="{{ old() }}">
         </label>
         <input type="submit">
     </form>
